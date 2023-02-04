9b4ce649c292283ecf24bd9d064c6f6f3da9ffdb
Beren Coates-Mullins <beren.coates-mullins@guestia.co.uk>
2023-02-04 15:14:34 +0000

    Actions working

---
+++ JavascriptComponents/Quill/QuillThemeType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSInterpDemo.JavascriptComponents.Quill;
+public enum QuillThemeType
+{
+    Snow,
+    Bubble,
+}
--- wwwroot/interpolate.js
+++ wwwroot/interpolate.js
@@ -6,3 +6,31 @@
 		timeout: timeoutInMS
 	}).show();
 }
+
+function initQuillRazorComponent(dotnetInstance, editorElement, theme, placeholder, isReadOnly, oldContent) {
+
+	var options = {
+		theme: theme,
+		readOnly: isReadOnly,
+		placeholder: placeholder
+	};
+
+	var editor = new Quill(editorElement, options);
+
+	try {
+		var parsedJson = JSON.parse(oldContent);
+		editor.setContents(parsedJson);
+	}
+	catch (e) {
+		editor.setText(oldContent);
+	}
+
+	editor.on('text-change', function () {
+		// get the contents of our editor
+		// stringify it
+		// send it to out dotnet instance to save
+		var jsonContents = editor.getContents();
+		var contents = JSON.stringify(jsonContents);
+		dotnetInstance.invokeMethodAsync('TextWasChanged', contents);
+	});
+}
