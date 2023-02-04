d6b489e8cf58b2ebbcf00e8bc7ed17cf19f50d7a
Beren Coates-Mullins <beren.coates-mullins@guestia.co.uk>
2023-02-04 14:32:35 +0000

    added document box

--- DatabaseContext.cs
+++ DatabaseContext.cs
@@ -9,4 +9,38 @@ namespace JSInterpDemo;
 public static class DatabaseContext
 {
 	public static List<Notification> Notifications { get; set; } = new List<Notification> { };
+
+	public static List<Document> Documents { get; set; } = new List<Document>
+	{
+		new Document
+		{
+			Id = 1,
+			Title = "Page Document 1",
+			PageContent = "This is page content",
+		},
+		new Document
+		{
+			Id = 2,
+			Title = "Page Document 2",
+            PageContent = "This is page content",
+        },
+		new Document
+		{
+			Id = 3,
+			Title = "Page Document 3",
+            PageContent = "This is page content",
+        },
+		new Document
+		{
+			Id = 4,
+			Title = "Page Document 4",
+            PageContent = "This is page content",
+        },
+		new Document
+		{
+			Id = 5,
+			Title = "Page Document 5",
+            PageContent = "This is page content",
+        },
+	};
 }
